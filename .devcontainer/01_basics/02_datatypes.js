5a7b2f6fc1f6b5ba9f6279996e04607ead562967
Gaurav Kalbhut <142148907+gauravkalbhut33@users.noreply.github.com>
2024-01-26 11:13:34 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat al JS code as newer version
+
+console.log(3+3);
+console.log("Om Sai Ram");
+
+let name = "Gaurav" // string
+let age = 23 // number
+let istrue = true // boolean
+let state = null;
+console.log(state);
+
+// null -> standalone value
+// undefined -> 
+// symbol -> unique
+
+// object
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null); // ->type is object
